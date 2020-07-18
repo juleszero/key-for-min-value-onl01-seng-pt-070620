ef850155ca1487677c4b97099f140ced833c8380
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-18 15:40:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(hash)
   min_key = nil
   min_value = nil
 hash.each do |name, number|
-if number < min_value
+if min_value == nil || number < min_value
   min_value = number
   min_key = name
   end
